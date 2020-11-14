a3f50024866b5373968e36f397a069cd921a9250
ThR3xx <thr3x.official@gmail.com>
2020-11-14 11:28:12 +0200

    .

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,6 +4,9 @@
 import Phaser from 'phaser';
 
 class Game extends Phaser.Scene {
+    preload(){
+        this.load.image('mage', 'assets/mage/mage.png');
+    }
     create(){
         let hero = this.physics.add.sprite(400; 300; "mage");
         hero.body.setCollideWorldBounds(true);
